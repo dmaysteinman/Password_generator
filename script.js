48bb3a61a563a55974c0e32e8e658fd9808135ef
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-10-11 17:53:14 -0400

    building functions

--- script.js
+++ script.js
@@ -1,6 +1,116 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// function getRandomIntInclusive(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+// }
+
+function randomInterger (num) {
+  return Math.floor(Math.random() * num) + 1;
+}
+
+function randomUpperCase () {
+  var lettersBig = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"]
+  var index = randomInterger(lettersBig.length - 1)
+  return lettersBig[index]
+}
+
+function randomLowerCase () {
+  var lettersSmall = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"]
+  var index = randomInterger(lettersSmall.length - 1)
+  return lettersSmall[index]
+}
+
+function randomSpecial () {
+  var special = [
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "^"
+    ]
+  var index = randomInterger(special.length - 1)
+  return special[index]
+}
+
+randomInterger(128)
+randomUpperCase()
+randomLowerCase()
+randomSpecial()
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +122,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Length
+//Capital Letters
+//Numbers
+//Special Characters
+//guaranteed characters array
+                            
+
+
+
