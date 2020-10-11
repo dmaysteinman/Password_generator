69ebee2b554a85fcd5e73d6cf2cae200ca7a5846
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-10-11 18:52:21 -0400

    it's coming together but it still needs work

--- script.js
+++ script.js
@@ -120,19 +120,21 @@ function randomSpecial() {
 // randomLowerCase()
 // randomSpecial()
 
+// function generatePassword()
+
 // Write password to the #password input
 function writePassword() {
   randomInterger(128);
   randomUpperCase();
   randomLowerCase();
   randomSpecial();
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
